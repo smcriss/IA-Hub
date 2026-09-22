dc0c42adc88cecfd4737b945278e2b50d5282014
Cris <smcristobal07@gmail.com>
2026-09-22 19:15:00 +0200

    Practicar tipos, condiciones y operadores

--- python-learning/02_variables.py
+++ python-learning/02_variables.py
@@ -3,5 +3,15 @@ name = input("¿Cómo te llamas? ")
 print("Hola,", name)
 print("Bienvenido a Python.")
 
-age = input("¿Cuántos años tienes, " + name + "? ")
-print("Genial,", name + ", tienes " + age + " años.")
+age = int(input("¿Cuántos años tienes, " + name + "? "))
+print(type(age))
+print(f"Genial, {name}, tienes {age} años.")
+
+print(age + 1)
+
+if age == 18:
+    print("Enhorabuena, acabas de cumplir la mayoría de edad.")
+elif age > 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
